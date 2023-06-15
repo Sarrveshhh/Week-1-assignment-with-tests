121cafbb391f4ffde29fbb57c7da25c138c21fdc
Sarrveshhh <sarrvesharyaa@gmail.com>
2023-06-16 02:59:34 +0530

    completed todo code with all passed testcases

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,6 +12,46 @@
 */
 
 class Todo {
+  constructor(){
+    this.list = [];
+  }
+
+  add(todo){
+    this.list.push(todo);
+  }
+
+  remove(index){
+    this.list.splice(index, 1)
+  }
+
+  clear(){
+    this.list = [];
+  }
+
+  getAll(){
+    return this.list;
+  }
+
+  get(index){
+
+    let n = this.list.length;
+
+    if(index > n-1){
+      return null;
+    }
+    return this.list[index];
+  }
+
+  update(index, string){
+    let n = this.list.length;
+
+    if(index > n-1){
+      return null;
+    }
+
+    this.list.splice(index, 1, string);
+
+  }
 
 }
 
